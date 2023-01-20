97cfccfae7b2a26a982632e1b58e6539080cba3d
mokumus <rocketpolka@gmail.com>
2023-01-20 16:17:25 +0300

    feat: featch todo lambda

---
+++ src/fetchTodos.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const AWS = require("aws-sdk")
+
+const fetchTodo = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient()
+
+  let todos;
+  try {
+    const results = await dynamodb.scan({ TableName: "TodoTable" }).promise()
+    todos = results.Items
+  } catch (error) {
+      console.log(error)
+  }
+
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(todos)
+  };
+};
+
+module.exports = {
+  handler: fetchTodo
+}
